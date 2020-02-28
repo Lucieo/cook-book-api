c7185be4a97518d3a01e10855e35266c3608e650
LucieO <luciecamilleory@gmail.com>
2020-02-28 17:39:33 +0100

    django app base ready

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,16 @@ ENV PYTHONUNBUFFERED 1
 #copy local file to docker image
 COPY ./requirements.txt /requirements.txt
 
+#install postegresql client apk(package manager of alpine) add(add a new package) --update(update registry before we add it) --no-cache (don't store registry index on our dockerfile = minimise docker container footprint)
+RUN apk add --update --no-cache postgresql-client
+#temporary requirements
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
+
 #install all requirements
 RUN pip install -r /requirements.txt
+#delete temporary requirements
+RUN apk del .tmp-build-deps
 
 #creates app empty folder on docker image
 RUN mkdir /app
---
+++ app/core/test/test_commands.py
@@ -0,0 +1,27 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # Mockup will return True + monitor how many times it was called
+            gi.return_value = True
+            call_command('wait_for_db')
+            # Check that get item was called once
+            self.assertEqual(gi.call_count, 1)
+
+    # patch decorator will pass extra argument to function
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # First 5 times it raises OperationalError
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
